d332f9e4da38b68fc873a7764c4adecc1053a3e7
BernardKwesi <bernarddormon31@gmail.com>
2022-11-14 07:21:02 +0000

    Add get implementation

--- linked-lists-Fourteen98/challenge.js
+++ linked-lists-Fourteen98/challenge.js
@@ -1,36 +1,57 @@
 class Node {
-	constructor(value, next_node = null) {
-		this.value = value;
-		this.next_node = next_node;
-	}
+  constructor(value, next_node = null) {
+    this.value = value;
+    this.next_node = next_node;
+  }
 }
 
 class LinkedList {
-	
-	constructor() {
-		let head = null;
-		let current = null;
-	}
+  constructor() {
+    this.head = null;
+    this.current = null;
+    this.length = 0;
+  }
 
-	add(number) {
-		// your code here
-		let node = new Node(number)
-		if (this.head == null) {
-			this.head = node;
-			return;
-		} else {
-			this.current = this.head
-			while (this.current.next_node) {
-				this.current = this.current.next_node
-			}
-			this.current.next_node = node;
-		}
-		
-	}
+  add(number) {
+    // your code here
+    let node = new Node(number);
+    if (this.head == null) {
+      this.head = node;
+      return;
+    } else {
+      this.current = this.head;
+      while (this.current.next_node) {
+        this.current = this.current.next_node;
+      }
+      this.current.next_node = node;
+      this.length++;
+      //console.log(this.current.next_node.value);
+    }
+  }
 
-	get(index) {
-		// your code here
-	}
+  get(index) {
+    // your code here
+    let counter = 0;
+    console.log(this.length);
+    if (this.head == null) {
+      return;
+    } else {
+      this.current = this.head;
+
+      while (counter != index) {
+        this.current = this.current.next_node;
+        console.log("counter" + counter);
+
+        // if (counter === index) {
+        //   return this.current.value;
+        // }
+
+        counter++;
+      }
+
+      return this.current.value;
+    }
+  }
 }
 
 const list = new LinkedList();
@@ -41,4 +62,3 @@ console.log(list.get(1));
 // // => 5
 
 // module.exports = LinkedList;
-
