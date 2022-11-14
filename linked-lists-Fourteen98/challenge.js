7c5513ea5c62d983fc52d218a8cbc579183ab535
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-14 06:54:56 +0000

    add implementation for add

--- linked-lists-Fourteen98/challenge.js
+++ linked-lists-Fourteen98/challenge.js
@@ -6,10 +6,26 @@ class Node {
 }
 
 class LinkedList {
-	// setup head and tail
+	
+	constructor() {
+		let head = null;
+		let current = null;
+	}
 
 	add(number) {
 		// your code here
+		let node = new Node(number)
+		if (this.head == null) {
+			this.head = node;
+			return;
+		} else {
+			this.current = this.head
+			while (this.current.next_node) {
+				this.current = this.current.next_node
+			}
+			this.current.next_node = node;
+		}
+		
 	}
 
 	get(index) {
@@ -22,6 +38,7 @@ const list = new LinkedList();
 list.add(3);
 list.add(5);
 console.log(list.get(1));
-// => 5
+// // => 5
 
-module.exports = LinkedList;
+// module.exports = LinkedList;
+
