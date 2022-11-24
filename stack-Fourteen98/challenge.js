e4e08489f4908ad808e8fe560e75f449d3d5821a
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-24 05:43:30 +0000

    add stack challenge

---
+++ stack-Fourteen98/challenge.js
@@ -0,0 +1,38 @@
+// Optionally: start with your code from LinkedList challenge.
+
+class Stack {
+  constructor () {
+    this.items = [];
+  }
+  push(number) {
+    // your code here
+    this.items.push(number)
+  }
+
+  pop() {
+    // your code here
+    if(this.items.length === 0) {
+      return null;
+    }
+    return this.items.pop()
+  }
+}
+
+const stack = new Stack()
+stack.push(3)
+stack.push(5)
+console.log(stack.pop())
+// => 5
+
+stack.push(2)
+stack.push(7)
+console.log(stack.pop())
+// => 7
+
+console.log(stack.pop())
+// => 2
+
+console.log(stack.pop())
+// => 3
+
+module.exports = Stack
