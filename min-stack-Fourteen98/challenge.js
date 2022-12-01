cfba1ce185d81068ad01484b62e175db3a151d53
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-12-01 06:01:16 +0000

    add min function

--- min-stack-Fourteen98/challenge.js
+++ min-stack-Fourteen98/challenge.js
@@ -29,9 +29,14 @@ class Stack {
 
   pop() {
     // your code here
-    if (this.head == null) return;
+    if (this.head == null) return 'empty';
     let current = this.head;
     let previous = this.head;
+    if (this.head.next_node == null) {
+      let headValue = this.head.value
+      this.head = null;
+      return headValue;
+    }
     while (current.next_node) {
       previous = current;
       current = current.next_node;
@@ -42,31 +47,34 @@ class Stack {
     this.length--;
     return popped;
   }
+
   min() {
     // your code here
-   
+    if (this.head == null) return 'empty';
+    let current = this.head
+    let minValue = this.head.value;
+    while (current.next_node) {
+      if (minValue > current.next_node.value) {
+        minValue = current.next_node.value;
+      }
+      current = current.next_node;
+    }
+    return minValue;
   }
 }
 
 const stack = new Stack()
 stack.push(3)
 stack.push(5)
-console.log(stack.pop())
-// => 3
+stack.push(1)
+stack.push(0)
 
-console.log(stack.pop())
+// console.log(stack.pop())
 // => 5
-stack.push(7)
-console.log(stack.pop)
-// console.log(stack.min())
+// console.log(stack.pop())
 // => 3
+// console.log(stack.pop())
 
-// stack.push(2)
-// console.log(stack.min())
-// => 2
-
-// stack.pop()
-// console.log(stack.min())
-// => 3
-
+console.log(stack.min())
 module.exports = Stack
+
