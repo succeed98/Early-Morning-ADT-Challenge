189d200e5be579c61e832433b1c085e03c281014
Fourteen98 <emuhiydeen6@gmail.com>
2022-12-05 05:29:19 +0000

    add for statements  to loop

--- balanced-brackets-Fourteen98/challenge.js
+++ balanced-brackets-Fourteen98/challenge.js
@@ -1,41 +1,52 @@
 function balancedBrackets(string) {
+    // Assumption
+    // [] => true
+    // [[{}]] => true
+    // {[} => false 
     // your code here
     let stack = [];
 
     // Traversing the Expression
-    for (let i = 0; i < string.length; i++) {
-        let x = string[i];
+    // for (let i = 0; i < string.length; i++) {
+    //     let x = string[i];
 
-        if (x === '(' || x === '[' || x === '{') {
-            stack.push(x);
+    //     if (x === '(' || x === '[' || x === '{') {
+    //         stack.push(x);
+    //         continue;
+    //     }
+
+    //     if (stack.length === 0)
+    //         return false;
+
+    //     let check;
+    //     switch (x) {
+    //         case ')':
+    //             check = stack.pop();
+    //             if (check === '{' || check === '[')
+    //                 return false;
+    //             break;
+
+    //         case '}':
+    //             check = stack.pop();
+    //             if (check === '(' || check === '[')
+    //                 return false;
+    //             break;
+
+    //         case ']':
+    //             check = stack.pop();
+    //             if (check === '(' || check === '{')
+    //                 return false;
+    //             break;
+    //     }
+    // }
+    // return (stack.length === 0);
+    for (let i = 0; i < string.length; i++){
+        // push if char is a closing bracets
+        if (string[i] === '[' || string[i] === '{' || string[i] === '(') {
+            stack.push(string[i]);
             continue;
         }
-
-        if (stack.length === 0)
-            return false;
-
-        let check;
-        switch (x) {
-            case ')':
-                check = stack.pop();
-                if (check === '{' || check === '[')
-                    return false;
-                break;
-
-            case '}':
-                check = stack.pop();
-                if (check === '(' || check === '[')
-                    return false;
-                break;
-
-            case ']':
-                check = stack.pop();
-                if (check === '(' || check === '{')
-                    return false;
-                break;
-        }
     }
-    return (stack.length === 0);
 }
 
 console.log(balancedBrackets('(hello)[world]'))
