a0ea8ea708618c3fc4de086ffaa5dc3dbbbc954e
BernardKwesi <bernarddormon31@gmail.com>
2022-12-13 05:29:10 +0000

    Implement binary search

--- binary-search-Fourteen98/challenge.js
+++ binary-search-Fourteen98/challenge.js
@@ -1,20 +1,22 @@
-module.exports = function sqrt(number) {
-  return sqrt_recursive(number, 0, number)
+function sqrt(number) {
+  return sqrt_recursive(number, 0, number);
 }
 
 function sqrt_recursive(number, min_interval, max_interval) {
   // write your code here
-  let mid_interval = (min_interval + max_interval) / 2;
-  if (mid_interval * mid_interval === number) {
-    return mid_interval;
-  } else if (mid_interval * mid_interval > number) {
-    return sqrt_recursive(number, min_interval, mid_interval);
-
+  let guess = (min_interval + max_interval) / 2;
+  if (guess * guess == number) {
+    return guess;
+  } else if (guess * guess < number) {
+    min_interval = guess + 1;
+    return sqrt_recursive(number, min_interval, max_interval);
   } else {
-    return sqrt_recursive(number, mid_interval, max_interval);
+    max_interval = guess - 1;
+    return sqrt_recursive(number, min_interval, max_interval);
   }
-
 }
 
-//console.log(sqrt(25))
-//console.log(sqrt(7056))
+console.log(sqrt(25));
+console.log(sqrt(7056));
+
+module.exports = sqrt;
